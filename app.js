f1045c5fc8a60f414173bbee033af70d4f6ccc69
Jon <lundbyjon@hotmail.com>
2023-02-20 23:55:53 +0100

    click funktioner virker ikke helt

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("javascrips says hi...")
+window.addEventListener("load", start);
+
+function start() {
+  console.log("start function called");
+  document
+    .querySelector("#bug_container")
+    .addEventListener("click", itemRemove);
+}
+
+function itemRemove() {
+  console.log("an item was clicked and removed");
+  document
+    .querySelector("bug_container")
+    .removeEventListener("click", itemRemove);
+  document.querySelector("#bug_container").classList.add("pause");
+  document.querySelector("#bug_sprite").classList.add("item_fadeout");
+}
