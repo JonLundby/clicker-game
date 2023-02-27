bd616e98216eed00583c369096d2c0065b33611b
Jon <lundbyjon@hotmail.com>
2023-02-28 00:10:10 +0100

    random item spawn

--- app.js
+++ app.js
@@ -1,78 +1,64 @@
 "use strict";
 
 //Man starter med 3 point. Hvis man rammer 0 så er det game over, hvis man rammer 7 så er det level complete.
-let points = 6;
+let points = 5;
 
 window.addEventListener("load", start);
 
 function start() {
   console.log("start function called...");
 
-  document.querySelector("#bug_container").classList.add("trash_items_movement");
-  document.querySelector("#plank_container").classList.add("trash_items_movement");
+  startAnimations();
+  startPositions();
+  registerClick();
 
-  document.querySelector("#bug_container").addEventListener("click", bugRemove);
-  document.querySelector("#plank_container").addEventListener("click", plankRemove);
+  document.querySelector("#plank1_container").addEventListener("animationiteration", plankRestart);
+  document.querySelector("#plank2_container").addEventListener("animationiteration", plankRestart);
+  document.querySelector("#bug3_container").addEventListener("animationiteration", bugRestart);
+  document.querySelector("#bug1_container").addEventListener("animationiteration", bugRestart);
+  document.querySelector("#bug2_container").addEventListener("animationiteration", bugRestart);
 }
 
-function bugRemove() {
-  //log checking if bug was clicked
-  console.log("bug clicked...");
-
-  //making the bug unclickable
-  document.querySelector("#bug_container").removeEventListener("click", bugRemove);
-
-  //adding pause and fadeout animation
-  document.querySelector("#bug_container").classList.add("pause");
-  document.querySelector("#bug_sprite").classList.add("item_fadeout");
-
-  //ending animation
-  document.querySelector("#bug_container").addEventListener("animationend", bugInstantiate);
-
-  //adding a point via removePoint function
-  removePoint();
-  gameOver();
+function startAnimations() {
+  document.querySelector("#plank1_container").classList.add("trash_items_movement");
+  document.querySelector("#plank2_container").classList.add("trash_items_movement");
+  document.querySelector("#bug1_container").classList.add("trash_items_movement");
+  document.querySelector("#bug2_container").classList.add("trash_items_movement");
+  document.querySelector("#bug3_container").classList.add("trash_items_movement");
 }
 
-//Remove point function
-function removePoint() {
-  points--;
-  console.log("points = " + points);
+function startPositions() {
+  document.querySelector("#plank1_container").classList.add("position2");
+  document.querySelector("#plank2_container").classList.add("position4");
+  document.querySelector("#bug1_container").classList.add("position1");
+  document.querySelector("#bug2_container").classList.add("position3");
+  document.querySelector("#bug3_container").classList.add("position5");
 }
 
-function bugInstantiate() {
-  //log message new bug instatiated
-  console.log("new bug is being instantiated...");
-
-  //removing the animationend
-  document.querySelector("#bug_container").removeEventListener("animationend", bugInstantiate);
-
-  //removing bugremove css class animations pause and fadeout
-  document.querySelector("#bug_container").classList.remove("pause");
-  document.querySelector("#bug_sprite").classList.remove("item_fadeout");
-
-  //restarting start animation classes
-  document.querySelector("#bug_container").classList.remove("trash_items_movement");
-  document.querySelector("#bug_container").offsetWidth;
-  document.querySelector("#bug_container").classList.add("trash_items_movement");
-
-  //reassigning eventlistener on "click"
-  document.querySelector("#bug_container").addEventListener("click", bugRemove);
+function registerClick() {
+  document.querySelector("#plank1_container").addEventListener("click", plankClicked);
+  document.querySelector("#plank2_container").addEventListener("click", plankClicked);
+  document.querySelector("#bug1_container").addEventListener("click", bugClicked);
+  document.querySelector("#bug2_container").addEventListener("click", bugClicked);
+  document.querySelector("#bug3_container").addEventListener("click", bugClicked);
 }
 
-function plankRemove() {
+function plankClicked() {
   //log checking if plank was clicked
   console.log("plank clicked...");
 
+  //cleaner code
+  let plank = this;
+
   //making the plank unclickable
-  document.querySelector("#plank_container").removeEventListener("click", plankRemove);
+  plank.removeEventListener("click", plankClicked);
 
   //adding pause and fadeout animation
-  document.querySelector("#plank_container").classList.add("pause");
-  document.querySelector("#plank_sprite").classList.add("item_fadeout");
+  plank.classList.add("pause");
+  plank.querySelector("img").classList.add("item_fadeout");
 
   //ending animation
-  document.querySelector("#plank_container").addEventListener("animationend", plankInstantiate);
+  plank.addEventListener("animationend", plankRemove);
 
   //Adding a point via addPoint function
   addPoint();
@@ -85,24 +71,102 @@ function addPoint() {
   console.log("points = " + points);
 }
 
-function plankInstantiate() {
+function plankRemove() {
   //log message new plank instatiated
   console.log("new plank is being instantiated...");
 
-  //removing the animationend
-  document.querySelector("#plank_container").removeEventListener("animationend", plankInstantiate);
+  //cleaner code
+  let plank = this; //document.querySelector("#plank1_container")
 
-  //removing bugremove css class animations pause and fadeout
-  document.querySelector("#plank_container").classList.remove("pause");
-  document.querySelector("#plank_sprite").classList.remove("item_fadeout");
+  //removing the animationend
+  plank.removeEventListener("animationend", plankRemove);
+
+  //removing bugClicked css class animations pause and fadeout
+  plank.classList.remove("pause");
+  plank.querySelector("img").classList.remove("item_fadeout");
 
   //restarting start animation classes
-  document.querySelector("#plank_container").classList.remove("trash_items_movement");
-  document.querySelector("#plank_container").offsetWidth;
-  document.querySelector("#plank_container").classList.add("trash_items_movement");
+  plankRestart.call(this);
 
   //reassigning eventlistener on "click"
-  document.querySelector("#plank_container").addEventListener("click", plankRemove);
+  plank.addEventListener("click", plankClicked);
+}
+
+function plankRestart() {
+  let plank = this;
+  console.log("plankRestart called");
+  plank.classList.remove("trash_items_movement");
+  plank.offsetWidth;
+  plank.classList.add("trash_items_movement");
+
+  plank.classList.remove("position1", "position2", "position3", "position4", "position5");
+
+  let pos = Math.floor(Math.random() * 5) + 1;
+  plank.classList.add("position" + pos);
+  console.log("plank repositioned at position" + pos);
+}
+
+function bugClicked() {
+  //log checking if bug was clicked
+  console.log("bug clicked...");
+
+  //cleaner code
+  let bug = this; //document.querySelector("#bug1_container")
+
+  //making the bug unclickable
+  bug.removeEventListener("click", bugClicked);
+
+  //adding pause and fadeout animation
+  bug.classList.add("pause");
+  bug.querySelector("img").classList.add("item_fadeout");
+
+  //ending animation
+  bug.addEventListener("animationend", bugRemove);
+
+  //adding a point via removePoint function
+  removePoint();
+  gameOver();
+}
+
+//Remove point function
+function removePoint() {
+  points--;
+  console.log("points = " + points);
+}
+
+function bugRemove() {
+  //log message new bug instatiated
+  console.log("new bug is being instantiated...");
+
+  //cleaner code
+  let bug = this; //document.querySelector("#bug1_container")
+
+  //removing the animationend
+  bug.removeEventListener("animationend", bugRemove);
+
+  //removing bugClicked css class animations pause and fadeout
+  bug.classList.remove("pause");
+  bug.querySelector("img").classList.remove("item_fadeout");
+
+  //restarting start animation classes
+  bugRestart.call(this);
+
+  //reassigning eventlistener on "click"
+  bug.addEventListener("click", bugClicked);
+}
+
+function bugRestart() {
+  let bug = this;
+  console.log("bugRestart was called");
+  bug.classList.remove("trash_items_movement");
+  bug.offsetWidth;
+  bug.classList.add("trash_items_movement");
+
+  bug.classList.remove("position1", "position2", "position3", "position4", "position5");
+
+  let pos = Math.floor(Math.random() * 5) + 1;
+  bug.classList.add("position" + pos);
+  console.log("bug repositioned at position" + pos);
 }
 
 function gameOver() {
@@ -117,6 +181,6 @@ function levelComplete() {
     console.log("Level complete should be visible...");
     document.querySelector("#level_complete").classList.remove("hidden");
     document.querySelector("#heaven_container").classList.remove("hidden");
-    document.querySelector("#heaven_container").classList.add("fade_in", "blur_pulse")
+    document.querySelector("#heaven_container").classList.add("fade_in", "blur_pulse");
   }
 }
